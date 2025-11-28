4ecedb729b92b34c05bf24b3ced4e5d82b3e2ddc
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-11-28 21:08:35 +0000

    feat(routes): create a route grouper with react router

---
+++ src/routes/appRoutes.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes } from "react-router"
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+
+      </Routes>
+    </Router>
+  )
+}
+
+export default AppRoutes;
