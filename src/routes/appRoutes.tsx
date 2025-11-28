e7fe663f66a23b33589df677de3b9ed87883893b
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-11-28 21:13:35 +0000

    feat(routes): add main routes to the grouper

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter as Router, Routes } from "react-router"
+import { Route, BrowserRouter as Router, Routes } from "react-router";
+import { MainRoutes } from "./mainRoutes";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-
+        <Route path="/*" element={ <MainRoutes /> } />
       </Routes>
     </Router>
   )
