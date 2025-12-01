9ef6f4cd2ab73000cf110ef12e7a59b1518ad4f5
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-01 11:49:25 +0000

    feat(pages): create the basic structure of the constructors page

---
+++ src/pages/constructors.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/layout/layout";
+
+const Constructors = () => {
+  return (
+    <Layout current_menu="Constructors">
+      <>
+      </>
+    </Layout>
+  )
+}
+
+export default Constructors;
