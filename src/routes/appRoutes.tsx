abeb880401ab9fa2d66158121107f76d655c1a42
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-01 11:51:00 +0000

    feat(routes): add the team route grouper to the main grouper with the prefix 'team'

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,11 +1,13 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router";
 import { MainRoutes } from "./mainRoutes";
+import { TeamRoutes } from "./teamRoutes";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/*" element={ <MainRoutes /> } />
+        <Route path="team/*" element={ <TeamRoutes /> } />
       </Routes>
     </Router>
   )
