6a176d0dca1acbb99641938609d6dc929199755a
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-01 16:49:19 +0000

    fix(api): correct f1Api dev URL and add timeout to axios client

--- src/api/axiosClients.ts
+++ src/api/axiosClients.ts
@@ -1,9 +1,11 @@
 import axios from "axios"
 
 export const openF1 = axios.create({
+  timeout: 10000,
   baseURL: "https://api.openf1.org/v1/"
 });
 
 export const devF1 = axios.create({
-  baseURL: "https://f1connectapi.vercel.app/"
+  timeout: 10000,
+  baseURL: "https://f1api.dev/api/",
 });
