1588dfd0190d78c3c5faf6d0f1b10bc06b0b41d9
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-01 11:49:09 +0000

    feat(pages): create the basic structure of the drivers page

---
+++ src/pages/drivers.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/layout/layout";
+
+const Drivers = () => {
+  return (
+    <Layout current_menu="Drivers">
+      <>
+      </>
+    </Layout>
+  )
+}
+
+export default Drivers;
