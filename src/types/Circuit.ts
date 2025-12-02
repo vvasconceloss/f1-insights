f39f1d4d605763d7990b7a653c13c28b5ec6032a
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-02 12:23:30 +0000

    feat(type): create a specific type for the circuit

---
+++ src/types/Circuit.ts
@@ -0,0 +1,14 @@
+export interface Circuit {
+  circuitId: string;
+  circuitName: string;
+  country: string;
+  city: string;
+  circuitLength: string;
+  corners: number;
+  firstParticipationYear: number;
+  lapRecord: string;
+  fastestLapDriverId: string;
+  fastestLapTeamId: string;
+  fastestLapYear: number;
+  url: string;
+}
