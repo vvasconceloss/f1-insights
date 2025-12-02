85f838ef041119bb676868f2705c4b5c0dfcd4b0
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-02 12:25:19 +0000

    feat(services): add typing to recent race result call

--- src/services/results.ts
+++ src/services/results.ts
@@ -1,6 +1,7 @@
 import { devF1 } from "@/api/axiosClients"
+import type { LastRaceResponse } from "@/types/Race";
 
-export const fetchLastRaceResult = async (signal?: AbortSignal) => {
-  const response = await devF1.get("current/last/race", { signal });
+export const fetchLastRaceResult = async (signal?: AbortSignal): Promise<LastRaceResponse> => {
+  const response = await devF1.get<LastRaceResponse>("current/last/race", { signal });
   return response.data;
 }
