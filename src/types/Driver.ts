23036158bf0b7f5968b49ed479f3b36e4065182f
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-02 12:23:53 +0000

    feat(type): create a specific type for the driver

---
+++ src/types/Driver.ts
@@ -0,0 +1,10 @@
+export interface Driver {
+  driverId: string;
+  number: number;
+  shortName: string;
+  url: string;
+  name: string;
+  surname: string;
+  nationality: string;
+  birthday: string;
+}
