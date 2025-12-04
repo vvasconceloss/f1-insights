ef2a90bfe83b5bc50821dcd39a952411128a79fc
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-04 15:55:45 +0000

    feat(components): create a widget to display the final classification of the last race

---
+++ src/components/widgets/lastRaceStanding.tsx
@@ -0,0 +1,62 @@
+import { normalizeGrid } from "@/utils/driver/normalizeGrid";
+import { useLastRaceResult } from "@/hooks/useLastRaceResult";
+import { DriverMovement } from "@/components/drivers/driverMovement";
+import { normalizePosition } from "@/utils/driver/normalizePosition";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
+export const LastRaceStandingWidget = () => {
+  const { data, loading, error } = useLastRaceResult();
+
+  if (error) return <>{error}</>;
+  if (loading) return <>Loading…</>;
+
+  return (
+    <section>
+      <article className="py-5">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>POS</TableHead>
+              <TableHead>DRIVER</TableHead>
+              <TableHead>GR</TableHead>
+              <TableHead>PTS</TableHead>
+              <TableHead className="text-right">TIME</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data?.races.results.map((result) => {
+              const driverGrid = normalizeGrid(result.grid);
+              const driverPosition = normalizePosition(result.position);
+              const driverId = result.driver.driverId || `${result.driver.name}-${result.driver.surname}`;
+
+              return (
+                <TableRow key={driverId}>
+                  <TableCell>
+                    {driverPosition.type == "dnf" ? "-" : `${driverPosition.value}º`}
+                    {driverPosition.type == "number" && driverGrid.available && (
+                      <DriverMovement position={driverPosition.value!} grid={driverGrid.value!} />
+                    )}
+                  </TableCell>
+                  <TableCell className="flex items-center space-x-2">
+                    <img 
+                      className="object-contain w-8 h-8" 
+                      src={result.driver.country.flags.svg} 
+                      alt={result.driver.country.flags.alt} 
+                    />
+                    <div>
+                      <h2 className="uppercase">{result.driver.name} {result.driver.surname}</h2>
+                      <h2 className="text-xs text-zinc-500">{result.team.teamName}</h2>
+                    </div>
+                  </TableCell>
+                  <TableCell>{driverGrid.available ? driverGrid.value : "PL"}</TableCell>
+                  <TableCell>{result.points}</TableCell>
+                  <TableCell className="text-right">{result.time.includes("DNF") ? "DNF" : result.time}</TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </article>
+    </section>
+  )
+}
