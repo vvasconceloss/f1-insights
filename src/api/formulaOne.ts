b482b02db3ff385a94fb497ac5e6c009d08ff979
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-05 19:29:18 +0000

    feat(api): create a function to retrieve data on Open F1 drivers by their acronyms

--- src/api/formulaOne.ts
+++ src/api/formulaOne.ts
@@ -6,13 +6,15 @@ const devF1 = axios.create({
   baseURL: "https://f1api.dev/api/",
 });
 
-/*
 const openF1 = axios.create({
   timeout: 10000,
   baseURL: "https://api.openf1.org/v1/"
 });
-*/
 
 export const getLastRace = async (signal?: AbortSignal) => {
   return (await devF1.get<LastRace>("current/last/race", { signal })).data;
+}
+
+export const getDriverByAcronym = async (acronym: string, signal?: AbortSignal) => {
+  return (await openF1.get<any>(`drivers?name_acronym=${acronym}`, { signal })).data[0];
 }
