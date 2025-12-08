a1dfcabf66ce11d62647f2de3448a67fd4170e79
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-08 13:02:37 +0000

    feat(components): add race date to last result widget

--- src/components/widgets/lastRace.tsx
+++ src/components/widgets/lastRace.tsx
@@ -1,9 +1,16 @@
 import type { LastRace } from "@/types/Race"
 
 export const LastRaceWidget = ({ data }: { data: LastRace | null }) => {
+  const date = new Date(data?.races.date || "");
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
   return (
     <section>
       <article className="py-5 border-b-2">
+        <h3 className="w-72 mb-2 rounded-md text-lg text-zinc-600">
+          {months[date.getMonth()]} {date.getDate()}, {date.getFullYear()} /
+          Round {data?.races.round}
+        </h3>
         <h2 className="font-bold text-2xl uppercase">{data?.races.raceName}</h2>
         <h3 className="text-lg text-zinc-600">{data?.races.circuit.city} - {data?.races.circuit.circuitName}</h3>
       </article>
